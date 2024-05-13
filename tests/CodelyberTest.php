489039b6d74ae0deb263aa9afb98401a21f6083c
Rafa Gómez <rgomezcasas@gmail.com>
2024-05-13 14:44:10 +0200

    chore: upgrade to php 8.3

--- tests/CodelyberTest.php
+++ tests/CodelyberTest.php
@@ -5,15 +5,15 @@ declare(strict_types=1);
 namespace CodelyTv\Tests;
 
 use CodelyTv\Codelyber;
+use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
 final class CodelyberTest extends TestCase
 {
-    /** @test */
-    public function itShouldSayHelloWhenGreeting(): void
+    #[Test] public function itShouldSayHelloWhenGreeting(): void
     {
         $codelyber = new Codelyber("Javi");
 
-        self::assertEquals("CodelyTV", $codelyber->greet());
+        $this->assertEquals("CodelyTV", $codelyber->greet());
     }
 }
